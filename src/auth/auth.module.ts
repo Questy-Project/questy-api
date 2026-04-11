9cac9627bd16d63c6dd4181c7b323368215765ca
Falkort <lleclercq91@outlook.com>
2026-04-11 13:41:02 +0200

    feat: #80 création automatique avatar et parts   à l'inscription

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,11 +6,15 @@ import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Avatar } from '../avatar/entities/avatar.entity';
+import { Part } from '../parts/entities/parts.entity';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
+    TypeOrmModule.forFeature([Avatar, Part]),
     // forRootAsync attend que ConfigModule soit prêt avant de lire JWT_SECRET et JWT_EXPIRES_IN
     JwtModule.registerAsync({
       imports: [ConfigModule],
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -8,12 +8,20 @@ import * as bcrypt from 'bcrypt';
 import { UsersService } from '../users/users.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Avatar } from '../avatar/entities/avatar.entity';
+import { Part } from '../parts/entities/parts.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
+    @InjectRepository(Avatar)
+    private readonly avatarRepository: Repository<Avatar>,
+    @InjectRepository(Part)
+    private readonly partsRepository: Repository<Part>,
   ) {}
 
   async register(dto: RegisterDto) {
@@ -26,6 +34,10 @@ export class AuthService {
       email: dto.email,
       password: hashed,
     });
+
+    await this.avatarRepository.save(this.avatarRepository.create({userId: user.id}));
+
+    await this.partsRepository.save(this.partsRepository.create({userId: user.id}));
 
     return {
       access_token: this.jwtService.sign({ sub: user.id, email: user.email }),
