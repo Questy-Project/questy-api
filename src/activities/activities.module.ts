c9ccb171b68cb46e791610c244080157cddf0b0c
Falkort <lleclercq91@outlook.com>
2026-04-14 15:58:07 +0200

    feat: mise à jour automatique des stats après déclaration
      closes #95

--- src/activities/activities.module.ts
+++ src/activities/activities.module.ts
@@ -1,14 +1,20 @@
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Activity } from "./entities/activity.entity";
-import { ActivitiesSeeder } from "./activities.seeder";
-import { Module } from "@nestjs/common";
-import { ActivitiesController } from "./activities.controller";
-import { ActivitiesService } from "./activities.service";
-import { ActivityLog } from "./entities/activity-log.entity";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Activity } from './entities/activity.entity';
+import { ActivitiesSeeder } from './activities.seeder';
+import { Module } from '@nestjs/common';
+import { ActivitiesController } from './activities.controller';
+import { ActivitiesService } from './activities.service';
+import { ActivityLog } from './entities/activity-log.entity';
+import { AvatarModule } from '../avatar/avatar.module';
+import { PartsModule } from '../parts/parts.module';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Activity, ActivityLog])],
-    controllers:[ActivitiesController],
-    providers: [ActivitiesSeeder, ActivitiesService]
+  imports: [
+    TypeOrmModule.forFeature([Activity, ActivityLog]),
+    AvatarModule,  // expose AvatarService pour ActivitiesService
+    PartsModule,   // expose PartsService pour ActivitiesService
+  ],
+  controllers: [ActivitiesController],
+  providers: [ActivitiesSeeder, ActivitiesService],
 })
 export class ActivitiesModule {}
