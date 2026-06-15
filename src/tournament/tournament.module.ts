d743ba53cd5c705a72d6ecce66dd68caba091aea
Falkort <lleclercq91@outlook.com>
2026-06-15 18:28:44 +0200

    feat: US-20 ajouter l'entité et le module de rang mensuel

--- src/tournament/tournament.module.ts
+++ src/tournament/tournament.module.ts
@@ -5,11 +5,13 @@ import { TournamentWeeklyRank } from './entities/tournament-weekly-rank.entity';
 import { TournamentService } from './tournament.service';
 import { TournamentController } from './tournament.controller';
 import { AvatarModule } from '../avatar/avatar.module';
+import { RankModule } from '../rank/rank.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([TournamentCombat, TournamentWeeklyRank]),
     AvatarModule,
+    RankModule,
   ],
   controllers: [TournamentController],
   providers: [TournamentService],
